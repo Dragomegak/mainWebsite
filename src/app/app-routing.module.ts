54b3a6d7bb274cade6f30e0ab5549e5a66a560be
Dragomegak <stevenle6569@gmail.com>
2020-04-16 15:53:42 -0400

    added home component and routing to match

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:  "", pathMatch:  "full",redirectTo:  "home"},
+  {path: "home", component: HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
