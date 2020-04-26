e326b4aead1d070c7509a379f35146a82665a8e9
Dragomegak <stevenle6569@gmail.com>
2020-04-26 14:46:47 -0400

    Added routing to contact info

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ProjectsComponent } from './projects/projects.component'
+import { ContactInfoComponent} from './contact-info/contact-info.component'
 
 
 const routes: Routes = [
   {path:  "", pathMatch:  "full",redirectTo:  "home"},
   {path: "home", component: HomeComponent},
-  {path: "projects", component: ProjectsComponent}
+  {path: "projects", component: ProjectsComponent},
+  {path: "contact-info", component: ContactInfoComponent}
 ];
 
 @NgModule({
