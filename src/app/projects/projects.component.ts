4d69745a3ebdeb5d1638c1a98dd218ddcdeb5b42
Dragomegak <stevenle6569@gmail.com>
2020-05-01 11:55:30 -0400

    Implemented local json reading for modularity, may be changed to read from backend server if need be (needs styling)

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import projects from '../../assets/projectData/projects.json';
 
 @Component({
   selector: 'app-projects',
@@ -8,6 +9,14 @@ import { Component, OnInit } from '@angular/core';
 export class ProjectsComponent implements OnInit {
 
   constructor() { }
+
+  public projectList:{
+    name:string,
+    link:string,
+    description:string,
+    workDescription:string,
+    technologiesUsed:string,
+  }[] = projects;
 
   ngOnInit(): void {
   }
